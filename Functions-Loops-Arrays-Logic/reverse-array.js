d4abc2e6f7eeb18db5793ba48d2b8651ee669a3e
Stella Stacy <preciousafayam@gmail.com>
2025-09-16 07:01:35 -1200

    local changes

---
+++ Functions-Loops-Arrays-Logic/reverse-array.js
@@ -0,0 +1,8 @@
+let myArray=[1,2,3,4,5];
+let reversedArray= [];
+console.log("Input:", myArray);
+for(let i=myArray.length - 1; i>=0; i--){
+    reversedArray.push(myArray[i]);
+}
+console.log("Output:", reversedArray)
+
